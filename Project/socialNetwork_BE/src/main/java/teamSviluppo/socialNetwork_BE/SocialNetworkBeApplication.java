cbf386b0d918ba9dc6d695f4d1a2245ce07a738b
Luca Giacalone <luca_giacalone@libero.it>
2024-01-25 16:08:52 +0100

    create spring project

---
+++ Project/socialNetwork_BE/src/main/java/teamSviluppo/socialNetwork_BE/SocialNetworkBeApplication.java
@@ -0,0 +1,13 @@
+package teamSviluppo.socialNetwork_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkBeApplication.class, args);
+	}
+
+}
---
+++ Project/socialNetwork_BE/src/test/java/teamSviluppo/socialNetwork_BE/SocialNetworkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package teamSviluppo.socialNetwork_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
